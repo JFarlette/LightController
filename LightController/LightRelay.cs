6b9cdcc8ab2ab7134b6fa1382b1372bb089d84c3
JFarlette <jfarlette@hotmail.com>
2014-09-16 22:42:33 -0700

    Removed commented out relay code

--- LightController/LightRelay.cs
+++ LightController/LightRelay.cs
@@ -7,36 +7,26 @@ namespace JFarlette.LightController
 {
     class LightRelay : IRelay
     {
-        // Is m_state needed?  Can m_rp.Read() be used for for IsTurnedOn?
         public LightRelay(Cpu.Pin pin)
         {
-            m_rp = new OutputPort(pin, false/*m_state == LightState.On*/);
+            m_rp = new OutputPort(pin, false);
         }
 
         public void TurnOn()
         {
-            //m_state = LightState.On;
             m_rp.Write(true);
         }
 
         public void TurnOff()
         {
-            //m_state = LightState.Off;
             m_rp.Write(false);
         }
 
         public bool IsTurnedOn()
         {
-            //return m_state == LightState.On;
             return m_rp.Read();
         }
 
-        //enum LightState
-        //{
-        //    On, Off
-        //}
-
-        //LightState m_state = LightState.Off;
         OutputPort m_rp;
     }
 }
